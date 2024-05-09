e511fd326df69b0403e9ec6bdad52da865b72034
Madan <Madanreddy319@gmail.com>
2024-05-09 20:24:44 +0100

    add the code

---
+++ src/main/java/Appium.java
@@ -0,0 +1,56 @@
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class Appium {
+
+    /// launch the appium
+    // connect the device to the machine
+
+
+    @Test
+
+    public void dd() throws IOException, InterruptedException {
+
+            String[] command = {"adb", "devices"};
+            ProcessBuilder processBuilder = new ProcessBuilder(command);
+            processBuilder.environment().put("ANDROID_ADB_SERVER_HOST", "host.docker.internal");
+            Process process = processBuilder.start();
+
+            // Read the output of the command
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            StringBuilder output = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line);
+            }
+
+            // Wait for the command to finish and get the exit code
+            int exitCode = process.waitFor();
+
+            if (exitCode == 0) {
+                String devicesInfo = output.toString();
+                System.out.println(devicesInfo);
+                DesiredCapabilities cap = new DesiredCapabilities();
+                cap.setCapability("devicename", "Lenovo");
+                cap.setCapability("automationName", "UiAutomator2");
+                cap.setCapability("udid", "HGR4T8AG");
+                cap.setCapability("platformName", "Android");
+                cap.setCapability("platformVersion", "13");
+                cap.setCapability("appPackage", "com.google.android.apps.youtube.music");
+                cap.setCapability("appActivity", "com.google.android.apps.youtube.music.activities.MusicActivity");
+               cap.setCapability("remoteAdbHost", "host.docker.internal");
+                // Initialize Appium driver
+                AppiumDriver driver = new AppiumDriver(new URL("http://0.0.0.0:4723/"), cap);
+            }
+
+            System.out.println("driver initiated");
+
+
+    }
+
+}
